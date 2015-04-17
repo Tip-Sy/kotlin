84cdb424102740f495713e72d99dcef22767c4f4
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-04-17 22:42:08 +0300

    Rename *array() factory methods to *arrayOf(). Rename copyToArray() to toArray().

--- js/js.libraries/src/core/kotlin.kt
+++ js/js.libraries/src/core/kotlin.kt
@@ -2,34 +2,64 @@ package kotlin
 
 import java.util.*
 
+deprecated("Use arrayOf() instead.")
 library
 public fun <T> array(vararg value : T): Array<T> = noImpl
+library
+public fun <T> arrayOf(vararg value : T): Array<T> = noImpl
 
 // "constructors" for primitive types array
 
+deprecated("Use doubleArrayOf() instead.")
 library
 public fun doubleArray(vararg content : Double): DoubleArray    = noImpl
+library
+public fun doubleArrayOf(vararg content : Double): DoubleArray    = noImpl
 
+deprecated("Use floatArrayOf() instead.")
 library
 public fun floatArray(vararg content : Float): FloatArray       = noImpl
+library
+public fun floatArrayOf(vararg content : Float): FloatArray       = noImpl
 
+deprecated("Use longArrayOf() instead.")
 library
 public fun longArray(vararg content : Long): LongArray          = noImpl
+library
+public fun longArrayOf(vararg content : Long): LongArray          = noImpl
 
+deprecated("Use intArrayOf() instead.")
 library
 public fun intArray(vararg content : Int): IntArray             = noImpl
+library
+public fun intArrayOf(vararg content : Int): IntArray             = noImpl
 
+deprecated("Use charArrayOf() instead.")
 library
 public fun charArray(vararg content : Char): CharArray          = noImpl
+library
+public fun charArrayOf(vararg content : Char): CharArray          = noImpl
 
+deprecated("Use shortArrayOf() instead.")
 library
 public fun shortArray(vararg content : Short): ShortArray       = noImpl
+library
+public fun shortArrayOf(vararg content : Short): ShortArray       = noImpl
 
+deprecated("Use byteArrayOf() instead.")
 library
 public fun byteArray(vararg content : Byte): ByteArray          = noImpl
+library
+public fun byteArrayOf(vararg content : Byte): ByteArray          = noImpl
 
+deprecated("Use booleanArrayOf() instead.")
 library
 public fun booleanArray(vararg content : Boolean): BooleanArray = noImpl
+library
+public fun booleanArrayOf(vararg content : Boolean): BooleanArray = noImpl
 
+deprecated("Use toArray() instead.")
 library("copyToArray")
 public fun <reified T> Collection<T>.copyToArray(): Array<T> = noImpl
+library("copyToArray")
+public fun <reified T> Collection<T>.toArray(): Array<T> = noImpl
--- libraries/stdlib/src/kotlin/collections/ArraysJVM.kt
+++ libraries/stdlib/src/kotlin/collections/ArraysJVM.kt
@@ -9,47 +9,74 @@ import kotlin.jvm.internal.Intrinsic
 /**
  * Returns an array containing the specified elements.
  */
+[Intrinsic("kotlin.arrays.array")] public fun <reified T> arrayOf(vararg t : T) : Array<T> = t as Array<T>
+
+deprecated("Use arrayOf() instead.")
 [Intrinsic("kotlin.arrays.array")] public fun <reified T> array(vararg t : T) : Array<T> = t as Array<T>
 
 // "constructors" for primitive types array
 /**
  * Returns an array containing the specified [Double] numbers.
  */
+[Intrinsic("kotlin.arrays.array")] public fun doubleArrayOf(vararg content : Double) : DoubleArray    = content
+
+deprecated("Use doubleArrayOf() instead.")
 [Intrinsic("kotlin.arrays.array")] public fun doubleArray(vararg content : Double) : DoubleArray    = content
 
 /**
  * Returns an array containing the specified [Float] numbers.
  */
+[Intrinsic("kotlin.arrays.array")] public fun floatArrayOf(vararg content : Float) : FloatArray       = content
+
+deprecated("Use floatArrayOf() instead.")
 [Intrinsic("kotlin.arrays.array")] public fun floatArray(vararg content : Float) : FloatArray       = content
 
 /**
  * Returns an array containing the specified [Long] numbers.
  */
+[Intrinsic("kotlin.arrays.array")] public fun longArrayOf(vararg content : Long) : LongArray          = content
+
+deprecated("Use longArrayOf() instead.")
 [Intrinsic("kotlin.arrays.array")] public fun longArray(vararg content : Long) : LongArray          = content
 
 /**
  * Returns an array containing the specified [Int] numbers.
  */
+[Intrinsic("kotlin.arrays.array")] public fun intArrayOf(vararg content : Int) : IntArray             = content
+
+deprecated("Use intArrayOf() instead.")
 [Intrinsic("kotlin.arrays.array")] public fun intArray(vararg content : Int) : IntArray             = content
 
 /**
  * Returns an array containing the specified characters.
  */
+[Intrinsic("kotlin.arrays.array")] public fun charArrayOf(vararg content : Char) : CharArray          = content
+
+deprecated("Use charArrayOf() instead.")
 [Intrinsic("kotlin.arrays.array")] public fun charArray(vararg content : Char) : CharArray          = content
 
 /**
  * Returns an array containing the specified [Short] numbers.
  */
+[Intrinsic("kotlin.arrays.array")] public fun shortArrayOf(vararg content : Short) : ShortArray       = content
+
+deprecated("Use shortArrayOf() instead.")
 [Intrinsic("kotlin.arrays.array")] public fun shortArray(vararg content : Short) : ShortArray       = content
 
 /**
  * Returns an array containing the specified [Byte] numbers.
  */
+[Intrinsic("kotlin.arrays.array")] public fun byteArrayOf(vararg content : Byte) : ByteArray          = content
+
+deprecated("Use byteArrayOf() instead.")
 [Intrinsic("kotlin.arrays.array")] public fun byteArray(vararg content : Byte) : ByteArray          = content
 
 /**
  * Returns an array containing the specified boolean values.
  */
+[Intrinsic("kotlin.arrays.array")] public fun booleanArrayOf(vararg content : Boolean) : BooleanArray = content
+
+deprecated("Use booleanArrayOf() instead.")
 [Intrinsic("kotlin.arrays.array")] public fun booleanArray(vararg content : Boolean) : BooleanArray = content
 
 /**
@@ -78,9 +105,13 @@ public fun ByteArray.toString(charset: Charset): String = String(this, charset)
 /**
  * Returns an array containing the elements of this collection.
  */
+deprecated("Use toArray() instead.")
 [Intrinsic("kotlin.collections.copyToArray")] public fun <reified T> Collection<T>.copyToArray(): Array<T> =
         throw UnsupportedOperationException()
 
+[Intrinsic("kotlin.collections.copyToArray")] public fun <reified T> Collection<T>.toArray(): Array<T> =
+        throw UnsupportedOperationException()
+
 /** Returns the array if it's not null, or an empty array otherwise. */
-public inline fun <reified T> Array<out T>?.orEmpty(): Array<out T> = this ?: array<T>()
+public inline fun <reified T> Array<out T>?.orEmpty(): Array<out T> = this ?: arrayOf<T>()
 
